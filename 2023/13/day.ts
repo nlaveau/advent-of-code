82eb2142faf1e8db117924f2a4d359879cb5ff2e
Nicolas Laveau <n.laveau@criteo.com>
2023-12-14 18:29:19 +0100

    ✨ refactor 2023/13 using helpers

--- 2023/13/day.ts
+++ 2023/13/day.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import { zip, sum } from "lodash";
+import { Array2D } from "../../array2d";
 import { dayRunner } from "../../dayRunner";
 
 const testFilesPart1 = ["./test.txt"];
@@ -8,7 +9,7 @@ const inputFilePart1 = "./input.txt";
 const inputFilePart2 = "./input.txt";
 let testOnly = false;
 
-type PbInput = string[][][];
+type PbInput = Array2D<string>[];
 
 function readInput(file: string): PbInput {
     let input = fs.readFileSync(file).toString().split("\n").map(line => line.replace('\r', ''));
@@ -16,53 +17,54 @@ function readInput(file: string): PbInput {
         input = input.slice(0, -1);
     }
 
-    let res: string[][][] = [];
+    let res: Array2D<string>[] = [];
     let cut: number = input.findIndex(line => line === "");
     while (cut >= 0) {
-        res.push(input.slice(0, cut).map(row => row.split("")));
+        res.push(new Array2D(input.slice(0, cut).map(row => row.split(""))));
         input = input.slice(cut + 1);
         cut = input.findIndex(line => line === "");
     }
-    res.push(input.map(row => row.split("")));
+    res.push(new Array2D(input.map(row => row.split(""))));
 
     return res;
 }
 
-function findSymmetry(map: string[][], target: number) {
-    let horizontal = map[0].map(_ => 0).slice(0, -1);
-    let vertical = map.map(_ => 0).slice(0, -1);
-    let w = map[0].length;
-    let h = map.length;
-    for (let j = 0; j < h; j++) {
-        for (let i = 1; i < w; i++) {
-            if (horizontal[i - 1] > target) continue;
-            let len = Math.min(i, w - i);
-            let sb = map[j].slice(i - len, i);
-            let sa = map[j].slice(i, i + len).reverse();
-            horizontal[i - 1] += sum(zip(sa, sb).map(p => p[0] !== p[1] ? 1 : 0));
-        }
-    }
-    for (let i = 0; i < w; i++) {
-        let column = map.map(row => row[i]);
-        for (let j = 1; j < h; j++) {
-            if (vertical[j - 1] > target) continue;
-            let len = Math.min(j, h - j);
-            let sb = column.slice(j - len, j);
-            let sa = column.slice(j, j + len).reverse();
-            vertical[j - 1] += sum(zip(sa, sb).map(p => p[0] !== p[1] ? 1 : 0));
-        }
-    }
-    let hcount = sum(horizontal.map((b, x) => b === target ? x + 1 : -1).filter(x => x > 0));
-    let vcount = sum(vertical.map((b, x) => b === target ? x + 1 : -1).filter(x => x > 0));
-    return vcount * 100 + hcount;
+function updateDiscrepancies(discreplancies: number[], line: string[], target: number) {
+    let length = line.length;
+    return discreplancies.map((count, i) => {
+        if (count > target) return count;
+        let reflectionLength = Math.min(i + 1, length - i - 1);
+        let sliceLeft = line.slice(i + 1 - reflectionLength, i + 1);
+        let sliceRight = line.slice(i + 1, i + 1 + reflectionLength).reverse();
+        return count + sum(zip(sliceLeft, sliceRight).map(p => p[0] !== p[1] ? 1 : 0));
+    })
 }
 
+function countGoodDiscrepancies(discreplancies: number[], target: number) {
+    return sum(discreplancies.map((count, i) => count === target ? i + 1 : -1).filter(x => x > 0));
+}
+
+function findSymmetry(array: Array2D<string>, target: number) {
+    let horzDisc = array.getRow(0).slice(1).map(_ => 0);
+    let vertDisc = array.getColumn(0).slice(1).map(_ => 0);
+    let size = array.getSize();
+
+    for (let i = 0; i < size.x; i++) {
+        let column = array.getColumn(i);
+        vertDisc = updateDiscrepancies(vertDisc, column, target);
+    }
+    for (let j = 0; j < size.y; j++) {
+        let column = array.getRow(j);
+        horzDisc = updateDiscrepancies(horzDisc, column, target);
+    }
+    return countGoodDiscrepancies(vertDisc, target) * 100 + countGoodDiscrepancies(horzDisc, target);
+}
 
 function part1Solver(input: PbInput) {
     return sum(input.map(map => findSymmetry(map, 0)));
 }
 
-function part2Solver(input: PbInput): any {
+function part2Solver(input: PbInput) {
     return sum(input.map(map => findSymmetry(map, 1)));
 }
 
